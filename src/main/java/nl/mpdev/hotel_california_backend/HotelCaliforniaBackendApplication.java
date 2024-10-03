1dfb8e19fcd829cf10f7a9431fc7b3727444fab6
Maarten Postma <maarten.postma1000@gmail.com>
2024-10-03 12:46:09 +0200

    Initial commit

---
+++ src/main/java/nl/mpdev/hotel_california_backend/HotelCaliforniaBackendApplication.java
@@ -0,0 +1,13 @@
+package nl.mpdev.hotel_california_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelCaliforniaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelCaliforniaBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nl/mpdev/hotel_california_backend/HotelCaliforniaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.mpdev.hotel_california_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelCaliforniaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

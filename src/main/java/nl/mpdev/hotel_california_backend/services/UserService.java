5614d326d87a45fbd14384a1f97ec96f59e049a2
Maarten Postma <maarten.postma1000@gmail.com>
2024-10-06 23:53:04 +0200

    Add registerNewCustomUser endpoint

--- src/main/java/nl/mpdev/hotel_california_backend/services/UserService.java
+++ src/main/java/nl/mpdev/hotel_california_backend/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.HashSet;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 @Service
 public class UserService {
@@ -24,19 +25,28 @@ public class UserService {
     authorityBuilder.authority("ROLE_USER");
     authorities.add(authorityBuilder.build());
     entity = entity.toBuilder()
+      // Later i have to decode the password here with a password decoder
       .authorities(authorities)
       .build();
     return userRepository.save(entity);
   }
 
   public User registerNewCustomUser(User entity) {
-    Set<Authority> authorities = new HashSet<>();
-    Authority.AuthorityBuilder authorityBuilder = Authority.builder();
-    authorityBuilder.username(entity.getUsername());
-    authorityBuilder.authority("ROLE_USER");
-    authorities.add(authorityBuilder.build());
+    Set<Authority> updatedAuthorities = new HashSet<>();
+    if (!entity.getAuthorities().isEmpty()) {
+      User finalEntity = entity;
+      updatedAuthorities = entity.getAuthorities().stream()
+        .map(existingAuthority -> {
+          return Authority.builder()
+            .username(finalEntity.getUsername())
+            .authority(existingAuthority.getAuthority())
+            .build();
+        })
+        .collect(Collectors.toSet());
+    }
     entity = entity.toBuilder()
-      .authorities(authorities)
+      // Later i have to decode the password here with a password decoder
+      .authorities(updatedAuthorities)
       .build();
     return userRepository.save(entity);
   }

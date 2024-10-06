21478b869a66d8549ab2b4ddf77c38bd5a13edeb
Maarten Postma <maarten.postma1000@gmail.com>
2024-10-07 00:21:27 +0200

    Add start of get endpont

--- src/main/java/nl/mpdev/hotel_california_backend/controllers/ProfileController.java
+++ src/main/java/nl/mpdev/hotel_california_backend/controllers/ProfileController.java
@@ -1,9 +1,15 @@
 package nl.mpdev.hotel_california_backend.controllers;
 
+import nl.mpdev.hotel_california_backend.dtos.profiles.ProfileCompleteResponseDto;
 import nl.mpdev.hotel_california_backend.services.ProfileService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/api/v1/profiles")
 public class ProfileController {
 
   private final ProfileService profileService;
@@ -11,4 +17,13 @@ public class ProfileController {
   public ProfileController(ProfileService profileService) {
     this.profileService = profileService;
   }
+
+  // GET
+
+//  @GetMapping("{id")
+//  public ResponseEntity<ProfileCompleteResponseDto> getProfileById(@PathVariable Integer id) {
+//
+//    return ResponseEntity.ok().body(Object);
+//  }
+
 }

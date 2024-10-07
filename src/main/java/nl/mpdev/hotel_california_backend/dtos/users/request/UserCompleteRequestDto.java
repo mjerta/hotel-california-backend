256db0e5770fc23e6ea6caccf99ba1c85cfa5a84
Maarten Postma <maarten.postma1000@gmail.com>
2024-10-07 23:59:42 +0200

    Add validation for the user dtos

--- src/main/java/nl/mpdev/hotel_california_backend/dtos/users/request/UserCompleteRequestDto.java
+++ src/main/java/nl/mpdev/hotel_california_backend/dtos/users/request/UserCompleteRequestDto.java
@@ -2,6 +2,7 @@ package nl.mpdev.hotel_california_backend.dtos.users.request;
 
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 import lombok.Builder;
 import lombok.Getter;
 import nl.mpdev.hotel_california_backend.dtos.authorities.request.AuthorityCompleteRequestDto;
@@ -10,7 +11,11 @@ import java.util.Set;
 @Builder(toBuilder = true)
 @Getter
 public class UserCompleteRequestDto {
+  @NotEmpty(message = "Username cannot be empty")
+  @Size(min = 3, max = 20, message = "Username must be between 3 and 20 characters")
   private String username;
+  @NotEmpty(message = "Password cannot be empty")
+  @Size(min = 8, message = "Password must be at least 8 characters long")
   private String password;
   @NotEmpty(message = "Authority set cannot be empty")
   private Set<AuthorityCompleteRequestDto> authority;

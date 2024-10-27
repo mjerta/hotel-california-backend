a47c89ccc72fc0022fa4d1cf25593eb4ab93ff24
Maarten Postma <maarten.postma1000@gmail.com>
2024-10-27 18:03:53 +0100

    Added some methods to create a image meal on startup

--- src/main/java/nl/mpdev/hotel_california_backend/config/onstartup/DataInitializer.java
+++ src/main/java/nl/mpdev/hotel_california_backend/config/onstartup/DataInitializer.java
@@ -1,17 +1,23 @@
 package nl.mpdev.hotel_california_backend.config.onstartup;
 
 import nl.mpdev.hotel_california_backend.exceptions.RecordNotFoundException;
-import nl.mpdev.hotel_california_backend.models.Authority;
-import nl.mpdev.hotel_california_backend.models.Profile;
-import nl.mpdev.hotel_california_backend.models.User;
+import nl.mpdev.hotel_california_backend.models.*;
+import nl.mpdev.hotel_california_backend.repositories.MealImageRepository;
+import nl.mpdev.hotel_california_backend.repositories.MealRepository;
 import nl.mpdev.hotel_california_backend.repositories.ProfileRepository;
 import nl.mpdev.hotel_california_backend.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.util.StreamUtils;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -45,21 +51,54 @@ public class DataInitializer {
 
   private final UserRepository userRepository;
   private final ProfileRepository profileRepository;
+  private final MealImageRepository mealImageRepository;
 
-  public DataInitializer(UserRepository userRepository, ProfileRepository profileRepository) {
+  public DataInitializer(UserRepository userRepository, ProfileRepository profileRepository, MealImageRepository mealImageRepository) {
     this.userRepository = userRepository;
     this.profileRepository = profileRepository;
+    this.mealImageRepository = mealImageRepository;
   }
 
   @Bean
-  public CommandLineRunner loadData(PasswordEncoder passwordEncoder) {
+  public CommandLineRunner loadData(PasswordEncoder passwordEncoder, MealRepository mealRepository) {
     return args -> {
+      ImageMeal imageMeal = createMealsWithImages("images/pizza.jpg");
+      mealImageRepository.save(imageMeal);
       createUserWithRoles(superUser, passwordEncoder.encode(superUserPassword), "ROLE_ADMIN", "ROLE_MANAGER", "ROLE_STAFF", "ROLE_USER");
       createUserWithRoles(managerUser, passwordEncoder.encode(managerUserPassword), "ROLE_MANAGER", "ROLE_STAFF", "ROLE_USER");
       createUserWithRoles(chefUser, passwordEncoder.encode(chefUserPassword), "ROLE_STAFF", "ROLE_CHEF", "ROLE_USER");
       createUserWithRoles(staffUser, passwordEncoder.encode(staffUserPassword), "ROLE_STAFF", "ROLE_USER");
-      createUserWithRoles(regularUser, passwordEncoder.encode(regularUserPassword), profileRepository.findById(1).orElseThrow(() -> new RecordNotFoundException("No profile found")), "ROLE_USER");
+      createUserWithRoles(regularUser, passwordEncoder.encode(regularUserPassword),
+        profileRepository.findById(1).orElseThrow(() -> new RecordNotFoundException("No profile found")), "ROLE_USER");
     };
+  }
+
+  private ImageMeal createMealsWithImages(String imagePath) {
+      ClassPathResource imageResource = new ClassPathResource(imagePath);
+      if (imageResource.exists()) {
+        try (InputStream inputStream = imageResource.getInputStream()) {
+          byte[] imageBytes = inputStream.readAllBytes();
+          return buildImageMael(imageBytes, imageBytes.length, Files.probeContentType(Paths.get(imagePath)), Paths.get(imagePath).getFileName().toString());
+        } catch (Exception e) {
+          System.err.println("Failed to load image: " + imagePath);
+          e.printStackTrace();
+        }
+      } else {
+        System.err.println("Image not found: " + imagePath);
+      }
+      return null;
+  }
+
+
+  private static ImageMeal buildImageMael(byte[] imageBytes, long size, String contentType, String filename) {
+    contentType = (contentType != null) ? contentType : "image/jpeg";
+    ImageMeal imageMeal = ImageMeal.builder()
+      .data(imageBytes)
+      .size(size)
+      .contentType(contentType)
+      .name(filename)
+      .build();
+    return imageMeal;
   }
 
   public void createUserWithRoles(String user, String password, String... roles) {
@@ -77,7 +116,6 @@ public class DataInitializer {
     }
     saveUser(user, password, authorities, profile);
   }
-
 
   private void saveUser(String userName, String password, Set<Authority> superUserAuthorities) {
     User adminUser = User.builder()
